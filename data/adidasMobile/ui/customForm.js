1ec7a35843827e4d114f1c6ed2589e3f02e29ba5
ksy8230 <ksy8230@hanmail.net>
2017-05-04 08:50:54 +0900

    mobile convention update

--- data/adidasMobile/ui/customForm.js
+++ data/adidasMobile/ui/customForm.js
@@ -1,61 +1,121 @@
 module.exports = `
+    <style>
+        .chk_box1,
+        .rdo_box1{
+        	display: inline-block;
+        	position: relative;
+        	font-size:13px;
+        }
+        .chk_box1>input,
+        .rdo_box1>input{
+        	opacity: 0;
+        	position: absolute;
+        	top: 0;
+        	left: 0;
+        }
+        .chk_box1>input+label:before,.chk_box1>input+label:after,
+        .rdo_box1>input+label:before,.chk_box1>input+label:after{
+        	content:'';
+        	display:inline-block;
+        }
+
+        .chk_box1>input+label:after,
+        .rdo_box1>input+label:after{
+        	display: none;
+        }
+        .chk_box1>input+label:before{
+        	width: 20px;
+        	height: 20px;
+        	border:1px solid #b8b8b8;
+        	vertical-align: middle;
+        	margin-right: 5px;
+        }
+        .chk_box1.blue>input:checked+label:before{
+        	background: #027dbf;
+        	border-color: #027dbf;
+        }
+
+        .chk_box1.line_blue>input:checked+label:before{
+        	border-color: #027dbf;
+        }
+
+        .chk_box1>input:checked + label:after{
+        	content:'';
+        	width: 12px;
+        	height:5px;
+        	display:block;
+        	border-bottom:2px solid #000;
+        	border-left:2px solid #000;
+        	position: absolute;
+        	top:6px;
+            left:4px;
+        	transform:rotate(-45deg);
+        	-webkit-transform:rotate(-45deg);
+        }
+
+        .chk_box1.blue>input:checked+label:after{
+        	border-color: #fff;
+        }
+
+        .chk_box1.line_blue>input:checked+label:after{
+            border-color: #027dbf;
+        }
+        .rdo_box1>input+label:before{
+        	width: 20px;
+        	height: 20px;
+        	border-radius:20px;
+        	border:1px solid #b8b8b8;
+        	vertical-align: middle;
+        	margin-right: 5px;
+        }
+        .rdo_box1>input:checked + label:after{
+        	content:'';
+        	width: 10px;
+        	height:10px;
+        	border-radius:10px;
+        	display:block;
+        	background: #3a3a3a !important;
+        	position: absolute;
+        	top:6px;
+            left:6px;
+        }
+
+        .rdo_box1.blue>input+label:before{
+        	width: 20px;
+        	height: 20px;
+        	border-radius:20px;
+        	border:1px solid #ebebeb;
+        	background: #ebebeb;
+        	vertical-align: middle;
+        	margin-right: 5px;
+        }
+
+        .rdo_box1.blue>input + label:after{
+        	content:'';
+        	width: 10px;
+        	height:10px;
+        	border-radius:10px;
+        	display:block;
+        	background: #fff !important;
+        	position: absolute;
+        	top:6px;
+            left:6px;
+        }
+
+        .rdo_box1.blue>input:checked + label:after{
+        	background: #027dbf !important;
+        }
+
+    </style>
     <div class="code-wrap">
         <div class="prefix">체크박스</div>
         <div class="code-box">
             <pre class="brush: html">
                 // 기본 체크박스
-                <div class="input-ctm input-check-ctm">
-                    <input type="checkbox" id="check1">
-                    <label for="check1">
-                        <a class="ctm-box spt_bg" href="javascript:void(0)">
-                            <span class=" spt_bg"></span>
-                        </a>
-                        기본 체크박스
-                    </label>
-                </div>
-
-                // 체크 시 파란라인
-                <div class="input-ctm input-check-ctm ctm-check-blue">
-                    <input type="checkbox" id="check1-blue">
-                    <label for="check1-blue">
-                        <a class="ctm-box spt_bg" href="javascript:void(0)">
-                            <span class=" spt_bg"></span>
-                        </a>
-                        체크 시 파란색 라인
-                    </label>
-                </div>
-
-                // 작은 체크박스
-                <div class="input-ctm input-check-ctm small">
-                    <input type="checkbox" id="check2">
-                    <label for="check2">
-                        <a class="ctm-box spt_bg" href="javascript:void(0)">
-                            <span class=" spt_bg"></span>
-                        </a>
-                        작은 체크박스
-                    </label>
-                </div>
-
-                // 작고 checked 시 파란색 버전의 체크박스
-                <div class="input-ctm input-check-ctm small blue">
-                    <input type="checkbox" id="check1_rea3">
-                    <label for="check3">
-                        <a class="ctm-box spt_bg" href="javascript:void(0)">
-                            <span class=" spt_bg"></span>
-                        </a>
-                        타입1
-                    </label>
-                </div>
-
-                // checked 추가
-                // checked 는 input 태그에 넣어야 합니다.
-                <div class="input-ctm input-check-ctm">
-                    <input type="checkbox" id="check_checked" checked>
-                    <label for="check_checked">
-                        <a class="ctm-box spt_bg" href="javascript:void(0)">
-                            <span class=" spt_bg"></span>
-                        </a>
-                        checked 속성
+                <div class="chk_box1">
+                    <input type="checkbox" id="cart_prod_all">
+                    <label for="cart_prod_all">
+                        전체
                     </label>
                 </div>
 
@@ -64,56 +124,10 @@ module.exports = `
 
         <div class="result_box">
 
-            <div class="input-ctm input-check-ctm" style="margin-right:40px;">
-                <input type="checkbox" id="check1_real">
-                <label for="check1_real">
-                    <a class="ctm-box spt_bg" href="javascript:void(0)">
-                        <span class=" spt_bg"></span>
-                    </a>
-                    타입1
-                </label>
-            </div>
-
-            <div class="input-ctm input-check-ctm ctm-check-blue" style="margin-right:40px;">
-                <input type="checkbox" id="check1-blue">
-                <label for="check1-blue">
-                    <a class="ctm-box spt_bg" href="javascript:void(0)">
-                        <span class=" spt_bg"></span>
-                    </a>
-                    타입2
-                </label>
-            </div>
-
-
-
-
-            <div class="input-ctm input-check-ctm small" style="margin-right:40px;">
-                <input type="checkbox" id="check1_rea2">
-                <label for="check1_rea2">
-                    <a class="ctm-box spt_bg" href="javascript:void(0)">
-                        <span class=" spt_bg"></span>
-                    </a>
-                    타입3
-                </label>
-            </div>
-
-            <div class="input-ctm input-check-ctm small blue" style="margin-right:40px;">
-                <input type="checkbox" id="check1_rea3">
-                <label for="check1_rea3">
-                    <a class="ctm-box spt_bg" href="javascript:void(0)">
-                        <span class=" spt_bg"></span>
-                    </a>
-                    타입4
-                </label>
-            </div>
-
-            <div class="input-ctm input-check-ctm">
-                <input type="checkbox" id="check_checked_real" checked>
-                <label for="check_checked_real">
-                    <a class="ctm-box spt_bg" href="javascript:void(0)">
-                        <span class=" spt_bg"></span>
-                    </a>
-                    checked 속성
+            <div class="chk_box1">
+                <input type="checkbox" id="cart_prod_all">
+                <label for="cart_prod_all">
+                    전체
                 </label>
             </div>
 
@@ -126,42 +140,26 @@ module.exports = `
         <div class="prefix">라디오버튼</div>
         <div class="code-box">
             <pre class="brush: html">
-                <div class="input-ctm input-radio-ctm">
-                    <input type="radio" id="radio1" name="radio_group" value="OPS1">
-                    <label for="radio1"><span class="ctm-box spt_bg"><span class="spt_bg"></span></span>선택1</label>
+                // 기본 체크박스
+                <div class="rdo_box1">
+                    <input type="radio" id="radio_real2" name="radio_group" value="OPS2">
+                    <label for="cart_prod_all">
+                        전체
+                    </label>
                 </div>
-
-                <div class="input-ctm input-radio-ctm">
-                    <input type="radio" id="radio2" name="radio_group" value="OPS2">
-                    <label for="radio2"><span class="ctm-box spt_bg"><span class="spt_bg"></span></span>선택2</label>
-                </div>
-
-                // checked 추가
-                // checked 는 input 태그에 넣어야 합니다.
-                <div class="input-ctm input-radio-ctm">
-                    <input type="radio" id="radio3" name="radio_group" value="OPS3" checked>
-                    <label for="radio3"><span class="ctm-box spt_bg"><span class="spt_bg"></span></span>선택3</label>
-                </div>
-
             </pre>
         </div>
 
         <div class="result_box">
 
-            <div class="input-ctm input-radio-ctm" style="margin-right:40px;">
-                <input type="radio" id="radio_real1" name="radio_group" value="OPS1">
-                <label for="radio_real1"><span class="ctm-box spt_bg"><span class="spt_bg"></span></span>선택1</label>
-            </div>
-
-            <div class="input-ctm input-radio-ctm" style="margin-right:40px;">
-                <input type="radio" id="radio_real2" name="radio_group" value="OPS2">
-                <label for="radio_real2"><span class="ctm-box spt_bg"><span class="spt_bg"></span></span>선택2</label>
-            </div>
-
-
-            <div class="input-ctm input-radio-ctm">
-                <input type="radio" id="radio_real3" name="radio_group" value="OPS3" checked>
-                <label for="radio_real3"><span class="ctm-box spt_bg"><span class="spt_bg"></span></span>선택3</label>
+            <div class="rdo_box1 blue">
+				<input type="radio" id="order_mem" name="order_choice" value="Y" checked="">
+				<label for="order_mem" class="adipro-r" id="loginLabel">로그인</label>
+			</div>
+            
+            <div class="rdo_box1 blue">
+                <input type="radio" id="order_mem2" name="order_choice" value="Y" checked="">
+                <label for="order_mem2" class="adipro-r" id="loginLabel">로그인</label>
             </div>
 
         </div>
--- data/adidasMobile/ui/customSelect.js
+++ data/adidasMobile/ui/customSelect.js
@@ -1,130 +1,213 @@
 module.exports = `
+    <style>
+        .sel_boxtype{
+            position: relative;
+        }
+        .sel_boxtype select{
+            height: 45px;
+        }
+        /* image slide left,right button */
+        .slide_btn_left,.slide_btn_right{
+        	width: 40px;
+        	height: 50px;
+        	display: block;
+        	position: absolute;
+        	top: 0;
+        }
+        .slide_btn_left{
+        	left: 0;
+        }
+        .slide_btn_right{
+        	right: 0;
+        }
+        .slide_btn_left:before,.slide_btn_right:before,
+        .slide_btn_left:after,.slide_btn_right:after{
+        	content:'';
+        	display: block;
+        	position: absolute;
+        	width: 40%;
+        	height: 1px;
+        	background: #fff;
+        	position: absolute;
+        	left: 50%;
+        	top: 50%;
+
+        	transform:rotate(45deg) translate3d(-50%,-50%,0);
+        	-webkit-transform:rotate(45deg) translate3d(-50%,-50%,0);
+        }
+        .slide_btn_right:after{
+        	transform:rotate(-45deg) translate3d(-50%,-50%,0);
+        	-webkit-transform:rotate(-45deg) translate3d(-50%,-50%,0);
+        }
+        .slide_btn_left:after,.slide_btn_left:before{
+        	transform-origin:-50% 50%;
+        	-webkit-transform-origin:-50% 50%;
+        }
+        .slide_btn_left:after{
+        	transform:rotate(-45deg) translate3d(-50%,-50%,0);
+        	-webkit-transform:rotate(-45deg) translate3d(-50%,-50%,0);
+        }
+        /* design select box - common type */
+        .sel_design{
+        	position: relative;
+        	box-sizing:border-box;
+        	-webkit-box-sizing:border-box;
+        }
+
+        .sel_design>a{
+        	width: 100%;
+        	height: 100%;
+        	padding:15px 0 0 10px;
+        	position: absolute;
+        	top: 0;
+        	left: 0;
+        	z-index:5;
+        	border:1px solid #e6e6e6;
+        	box-sizing:border-box;
+        	-webkit-box-sizing:border-box;
+        	color:#000;
+        	font-size:12px;
+        	background: #fff;
+        }
+
+        .sel_design.focused>a{
+        	/*background: #000;*/
+        	/*border:4px solid #000;*/
+        	-webkit-animation: size-error-blink 1s ease 5;
+        	-webkit-animation-iteration-count:infinite;
+        	animation: size-error-blink 1s ease 5;
+        	animation-iteration-count:infinite;
+        }
+
+        .sel_design.focused_line::before{
+        	content:'';
+        	width: 100%;
+        	height: 100%;
+        	box-sizing: border-box;
+        	-webkit-box-sizing: border-box;
+        	border: 3px solid #027dbf;
+        	position: absolute;
+        	top: 0;
+        	left: 0;
+        	opacity: 0;
+        	z-index: 9;
+
+        	-webkit-animation: size-error-line 1s ease 5;
+        	-webkit-animation-iteration-count:infinite;
+        	animation: size-error-line 1s ease 5;
+        	animation-iteration-count:infinite;
+        }
+
+        .sel_design>select{
+        	width: 100%;
+        	-webkit-appearance: none;
+        	-moz-appearance: none;
+        	border-radius:0;
+        	border-width:0;
+        	color: #000;
+        	position: relative;
+        	opacity: 0;
+        	z-index:10
+        }
+
+        .sel_design>select:disabled+a{
+        	background: #eee;
+        	color: #999;
+        }
+
+        .sel_design>select:disabled+a::before{
+        	background: #eee;
+        }
+
+        .sel_design>select:disabled+a::after{
+        	border-top-color: #ccc;
+        }
+
+        .sel_design>select:disabled+a.toggle_arw::before,
+        .sel_design>select:disabled+a.toggle_arw::after{
+        	background: #ccc;
+        }
+
+        /*  toggle +/-  button */
+        .toggle_arw:before,
+        .toggle_arw:after{
+        	transform-origin:50% 50%;
+        	-webkit-transform-origin:50% 50%;
+        	content:'';
+        	width:12px;
+        	height:4px;
+        	background: #000;
+        	position: absolute;
+        	top:50%;
+        	box-sizing:border-box;
+        	-webkit-box-sizing:border-box;
+        	transition:all 0.3s cubic-bezier(1,0,0,1);
+        	-webkit-transition:all 0.3s cubic-bezier(1,0,0,1);
+        }
+
+        .toggle_arw:before{
+        	right:21px;
+        	transform:rotate(45deg);
+        	-webkit-transform:rotate(45deg);
+        }
+
+        .toggle_arw:after{
+        	right:15px;
+        	transform:rotate(-45deg);
+        	-webkit-transform:rotate(-45deg);
+        }
+
+        .toggle_arw.open:before{
+        	right:21px;
+        	transform:rotate(-45deg);
+        	-webkit-transform:rotate(-45deg);
+        }
+
+        .toggle_arw.open:after{
+        	right:15px;
+        	transform:rotate(45deg);
+        	-webkit-transform:rotate(45deg);
+        }
+
+    </style>
     <div class="code-wrap">
         <div class="prefix">기본 셀렉트박스</div>
         <div class="code-box">
             <pre class="brush: html">
-                <div class="selectbox-ctm">
-                    <a href="javascript:void(0)">셀렉트 박스</a>
-                    <div class="arrow spt_bg"></div>
-                    <div class="select-contents option-list">
-                    	<ul>
-                        	<li data-option-value="value1"><a href="javascript:void(0)">옵션1</a></li>
-                            <li data-option-value="value2"><a href="javascript:void(0)">옵션2</a></li>
-                            <li data-option-value="값3"><a href="javascript:void(0)">옵션3</a></li>
-                            <li data-option-value="값4"><a href="javascript:void(0)">옵션4</a></li>
-                        </ul>
-                    </div>
+                <div class="sel_boxtype sel_design">
+                    <a href="#" class="toggle_arw"><span>베스트순</span></a>
+                    <select name="order_by" id="order_by" onchange="javascript:fnSearch();">
+                        <option value="1" selected="">베스트순</option>
+                        <option value="2">신상품순</option>
+                        <option value="5">상품평순</option>
+                        <option value="3">높은가격순</option>
+                        <option value="4">낮은가격순</option>
+                    </select>
                 </div>
             </pre>
         </div>
 
         <div class="result_box" style="width:400px;">
-            <div class="selectbox-ctm">
-                <a href="javascript:void(0)">셀렉트 박스</a>
-                <div class="arrow spt_bg"></div>
-                <div class="select-contents option-list">
-                    <ul>
-                        <li data-option-value="value1"><a href="javascript:void(0)">옵션1</a></li>
-                        <li data-option-value="value2"><a href="javascript:void(0)">옵션2</a></li>
-                        <li data-option-value="값3"><a href="javascript:void(0)">옵션3</a></li>
-                        <li data-option-value="값4"><a href="javascript:void(0)">옵션4</a></li>
-                    </ul>
-                </div>
+            <div class="sel_boxtype sel_design">
+                <a href="#" class="toggle_arw"><span>베스트순</span></a>
+                <select name="order_by" id="order_by" onchange="javascript:fnSearch();">
+                    <option value="1" selected="">베스트순</option>
+                    <option value="2">신상품순</option>
+                    <option value="5">상품평순</option>
+                    <option value="3">높은가격순</option>
+                    <option value="4">낮은가격순</option>
+                </select>
             </div>
         </div>
     </div>
-    <div class="code-wrap">
-        <div class="prefix">파란색 화살표 셀렉트 박스</div>
-        <div class="code-box">
-            <pre class="brush: html">
-                <div class="selectbox-ctm blue">
-                    <a href="javascript:void(0)">셀렉트 박스</a>
-                    <div class="arrow spt_bg"></div>
-                    <div class="select-contents option-list">
-                    	<ul>
-                        	<li data-option-value="value1"><a href="javascript:void(0)">옵션1</a></li>
-                            <li data-option-value="value2"><a href="javascript:void(0)">옵션2</a></li>
-                            <li data-option-value="값3"><a href="javascript:void(0)">옵션3</a></li>
-                            <li data-option-value="값4"><a href="javascript:void(0)">옵션4</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </pre>
-        </div>
-        <div class="result_box" style="width:400px;">
-            <div class="selectbox-ctm blue">
-                <a href="javascript:void(0)">파란색 화살표 셀렉트 박스</a>
-                <div class="arrow spt_bg"></div>
-                <div class="select-contents option-list">
-                    <ul>
-                        <li data-option-value="value1"><a href="javascript:void(0)">옵션1</a></li>
-                        <li data-option-value="value2"><a href="javascript:void(0)">옵션2</a></li>
-                        <li data-option-value="값3"><a href="javascript:void(0)">옵션3</a></li>
-                        <li data-option-value="값4"><a href="javascript:void(0)">옵션4</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="code-wrap">
-        <div class="prefix">callback 이벤트 설정하기</div>
-        <div class="code-box">
-            <pre class="brush: html">
-                <div class="selectbox-ctm blue" id="bind-selectbox">
-                    <a href="javascript:void(0)">이벤트 바인딩 셀렉트 박스</a>
-                    <div class="arrow spt_bg"></div>
-                    <div class="select-contents option-list">
-                    	<ul>
-                        	<li data-option-value="value1"><a href="javascript:void(0)">옵션1</a></li>
-                            <li data-option-value="value2"><a href="javascript:void(0)">옵션2</a></li>
-                            <li data-option-value="값3"><a href="javascript:void(0)">옵션3</a></li>
-                            <li data-option-value="값4"><a href="javascript:void(0)">옵션4</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </pre>
-        </div>
-        <div class="code-box">
-            <pre class="brush: js">
-                $('#bind-selectbox').ctmSelectSetCallback({
-                    //클릭 적용 전
-                    before : function(currentValue , index){
-                        alert('값 : '+currentValue);
-                        console.log(currentValue , index); //클릭 한 value 값 , li태그 index값
-                        console.log(this); //selected 클래스가 추가 안된 현재 li 태그
-                    },
-                    //클릭 적용 후
-                    after : function(currentValue , index){
-                        alert('값 : '+currentValue);
-                        console.log(currentValue , index); //클릭 한 value 값 , li태그 index값
-                        console.log(this); //selected 클래스가 추가 된 현재 li 태그
-                    }
-                });
-            </pre>
-        </div>
-        <div class="result_box" style="width:400px;">
-            <div class="selectbox-ctm" id="bind-selectbox-result">
-                <a href="javascript:void(0)">이벤트 바인딩 셀렉트 박스</a>
-                <div class="arrow spt_bg"></div>
-                <div class="select-contents option-list">
-                    <ul>
-                        <li data-option-value="value1"><a href="javascript:void(0)">옵션1</a></li>
-                        <li data-option-value="value2"><a href="javascript:void(0)">옵션2</a></li>
-                        <li data-option-value="값3"><a href="javascript:void(0)">옵션3</a></li>
-                        <li data-option-value="값4"><a href="javascript:void(0)">옵션4</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
 
 
-    </div>
 
     <script type="text/javascript" src="//image.adidas.co.kr/js/customForm.js"></script>
     <script type="text/javascript">
-        $.fn.cmtSelectInit();
+        $.fn.fnSearch();
 
-        $('#bind-selectbox-result').ctmSelectSetCallback({
+        $('#bind-selectbox-result').fnSearch({
             before : function(currentValue , index){
                 console.log(currentValue , index);
                 console.log(this);
