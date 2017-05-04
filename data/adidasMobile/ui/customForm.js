6e59daec954cff74c2b83674b7894f130b18132d
ksy8230 <ksy8230@hanmail.net>
2017-05-04 17:37:25 +0900

    mobile convention update

--- data/adidasMobile/ui/customForm.js
+++ data/adidasMobile/ui/customForm.js
@@ -113,21 +113,34 @@ module.exports = `
             <pre class="brush: html">
                 // 기본 체크박스
                 <div class="chk_box1">
-                    <input type="checkbox" id="cart_prod_all">
-                    <label for="cart_prod_all">
-                        전체
+                    <input type="checkbox" id="check_basic">
+                    <label for="check_basic">
+                        타입1
                     </label>
                 </div>
-
+                // 체크시 파란박스
+                <div class="chk_box1 blue">
+                    <input type="checkbox" id="check_blue">
+                    <label for="check_blue">
+                        타입2
+                    </label>
+                </div>
             </pre>
         </div>
 
         <div class="result_box">
 
-            <div class="chk_box1">
-                <input type="checkbox" id="cart_prod_all">
-                <label for="cart_prod_all">
-                    전체
+            <div class="chk_box1" style="margin-right:40px;">
+                <input type="checkbox" id="check_basic">
+                <label for="check_basic">
+                    타입1
+                </label>
+            </div>
+
+            <div class="chk_box1 blue">
+                <input type="checkbox" id="check_blue">
+                <label for="check_blue">
+                    타입2
                 </label>
             </div>
 
@@ -140,26 +153,31 @@ module.exports = `
         <div class="prefix">라디오버튼</div>
         <div class="code-box">
             <pre class="brush: html">
-                // 기본 체크박스
+                // 기본 라디오버튼
                 <div class="rdo_box1">
                     <input type="radio" id="radio_real2" name="radio_group" value="OPS2">
                     <label for="cart_prod_all">
-                        전체
+                        타입1
                     </label>
+                </div>
+                // 파란 라디오버튼
+                <div class="rdo_box1 blue">
+                    <input type="radio" id="order_mem2" name="order_choice" value="Y" checked="">
+                    <label for="order_mem2" class="adipro-r" id="loginLabel">타입2</label>
                 </div>
             </pre>
         </div>
 
         <div class="result_box">
 
-            <div class="rdo_box1 blue">
+            <div class="rdo_box1 blue" style="margin-right:40px;">
 				<input type="radio" id="order_mem" name="order_choice" value="Y" checked="">
-				<label for="order_mem" class="adipro-r" id="loginLabel">로그인</label>
+				<label for="order_mem" class="adipro-r" id="loginLabel">타입1</label>
 			</div>
-            
+
             <div class="rdo_box1 blue">
                 <input type="radio" id="order_mem2" name="order_choice" value="Y" checked="">
-                <label for="order_mem2" class="adipro-r" id="loginLabel">로그인</label>
+                <label for="order_mem2" class="adipro-r" id="loginLabel">타입2</label>
             </div>
 
         </div>
