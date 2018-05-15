0def743da7b57900c45532e0934eae920f09fa36
ksy8230 <ksy8230@hanmail.net>
2018-05-15 10:45:38 +0900

    mobile icon add

--- data/adidasMobile/common/iconImage.js
+++ data/adidasMobile/common/iconImage.js
@@ -2,9 +2,9 @@ module.exports = `
 
     <style>
 
-        .box{width:50%;float:left;height:350px;margin:0 0 30px 0;}
+        .box{width:50%;float:left;height:auto;margin:0 0 30px 0;}
         .prefix{margin:10px 0;}
-        .resultbox{padding:15px 0 15px;height:50px;background:#ddd;text-align:center;border:1px solid #ccc;}
+        .resultbox{padding:15px 0 15px;height:50px;background:#efefef;text-align:center;border:1px solid #ccc;}
 
         .ico_filiter{
             display:inline-block;
@@ -44,6 +44,76 @@ module.exports = `
             background-size: 50% 50%;
             background-position: center;
             background-repeat: no-repeat;
+        }
+        .ico_kakao_darker{
+            width: 30px;
+            height: 30px;
+            display: inline-block;
+            margin-right: 5px;
+            background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_kakao_gray_darker.png') 0 0 no-repeat;
+            -webkit-background-size: 30px auto;
+            background-size: 30px auto;
+            vertical-align: middle;
+        }
+        .ico_kakao_gray{
+            width: 30px;
+            height: 30px;
+            display: inline-block;
+            margin-right: 5px;
+            background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_kakao_gray.gif') 0 0 no-repeat;
+            -webkit-background-size: 30px auto;
+            background-size: 30px auto;
+            vertical-align: middle;
+        }
+        .ico_naver_darker{
+            width: 30px;
+            height: 30px;
+            display: inline-block;
+            margin-right: 5px;
+            background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_naver_gray_darker.png') 0 0 no-repeat;
+            -webkit-background-size: 30px auto;
+            background-size: 30px auto;
+            vertical-align: middle;
+        }
+        .ico_naver_gray{
+            width: 30px;
+            height: 30px;
+            display: inline-block;
+            margin-right: 5px;
+            background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_naver_gray.gif') 0 0 no-repeat;
+            -webkit-background-size: 30px auto;
+            background-size: 30px auto;
+            vertical-align: middle;
+        }
+        .ico_naver_white{
+            width: 30px;
+            height: 30px;
+            display: inline-block;
+            margin-right: 5px;
+            background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_naver_white.png') 0 0 no-repeat;
+            -webkit-background-size: 30px auto;
+            background-size: 30px auto;
+            vertical-align: middle;
+        }
+        .ico_kakao_brown{
+            width: 30px;
+            height: 30px;
+            display: inline-block;
+            margin-right: 5px;
+            background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_kakao.png') 0 0 no-repeat;
+            -webkit-background-size: 30px auto;
+            background-size: 30px auto;
+            vertical-align: middle;
+        }
+        .ico_naver_green{
+            width: 30px;
+            height: 30px;
+            display: inline-block;
+            margin-right: 5px;
+            background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_naver.gif') 0 0 no-repeat;
+            -webkit-background-size: 30px auto;
+            background-size: 30px auto;
+            vertical-align: middle;
         }
 
     </style>
@@ -125,6 +195,159 @@ module.exports = `
                         background-size: 50% 50%;
                         background-position: center;
                         background-repeat: no-repeat;
+                    }
+                </pre>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="prefix">카카오톡 아이콘 심플형(어두운 회색)</div>
+            <div class="used" style="margin:5px 0 15px 0; font-size:11px;">사용된 페이지 : Payment_login.html </div>
+            <div class="resultbox">
+                <span class="ico_kakao_darker"></span>
+            </div>
+            <div class="code-box">
+                <pre class="brush: css">
+                    .button_area a span.ico_kakao_darker{
+                        width: 16px;
+                        height: 16px;
+                        display: inline-block;
+                        margin-right: 5px;
+                        background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_kakao_gray_darker.png') 0 0 no-repeat;
+                        -webkit-background-size: 16px auto;
+                        background-size: 16px auto;
+                        vertical-align: middle;
+                    }
+                </pre>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="prefix">카카오톡 아이콘 심플형(회색)</div>
+            <div class="used" style="margin:5px 0 15px 0; font-size:11px;">사용된 페이지 : - </div>
+            <div class="resultbox">
+                <span class="ico_kakao_gray"></span>
+            </div>
+            <div class="code-box">
+                <pre class="brush: css">
+                    .button_area a span.ico_kakao{
+                        width: 16px;
+                        height: 16px;
+                        display: inline-block;
+                        margin-right: 5px;
+                        background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_kakao_gray.gif') 0 0 no-repeat;
+                        -webkit-background-size: 16px auto;
+                        background-size: 16px auto;
+                        vertical-align: middle;
+                    }
+                </pre>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="prefix">네이버 아이콘 심플형(어두운 회색)</div>
+            <div class="used" style="margin:5px 0 15px 0; font-size:11px;">사용된 페이지 : Payment_login.html </div>
+            <div class="resultbox">
+                <span class="ico_naver_darker"></span>
+            </div>
+            <div class="code-box">
+                <pre class="brush: css">
+                    .button_area a span.ico_naver_darker{
+                        width: 16px;
+                        height: 16px;
+                        display: inline-block;
+                        margin-right: 5px;
+                        background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_naver_gray_darker.png') 0 0 no-repeat;
+                        -webkit-background-size: 16px auto;
+                        background-size: 16px auto;
+                        vertical-align: middle;
+                    }
+                </pre>
+            </div>
+        </div>
+        <div class="box">
+            <div class="prefix">네이버 아이콘 심플형(회색)</div>
+            <div class="used" style="margin:5px 0 15px 0; font-size:11px;">사용된 페이지 : - </div>
+            <div class="resultbox">
+                <span class="ico_naver_gray"></span>
+            </div>
+            <div class="code-box">
+                <pre class="brush: css">
+                    .css{
+                        width: 16px;
+                        height: 16px;
+                        display: inline-block;
+                        margin-right: 5px;
+                        background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_naver_gray.gif') 0 0 no-repeat;
+                        -webkit-background-size: 16px auto;
+                        background-size: 16px auto;
+                        vertical-align: middle;
+                    }
+                </pre>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="prefix">네이버 아이콘 심플형(흰색)</div>
+            <div class="used" style="margin:5px 0 15px 0; font-size:11px;">사용된 페이지 : login.html </div>
+            <div class="resultbox">
+                <span class="ico_naver_white"></span>
+            </div>
+            <div class="code-box">
+                <pre class="brush: css">
+                    .css{
+                        width: 16px;
+                        height: 16px;
+                        display: inline-block;
+                        margin-right: 5px;
+                        background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_naver_white.png') 0 0 no-repeat;
+                        -webkit-background-size: 16px auto;
+                        background-size: 16px auto;
+                        vertical-align: middle;
+                    }
+                </pre>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="prefix">카카오 아이콘 심플형(갈색)</div>
+            <div class="used" style="margin:5px 0 15px 0; font-size:11px;">사용된 페이지 : login.html </div>
+            <div class="resultbox">
+                <span class="ico_kakao_brown"></span>
+            </div>
+            <div class="code-box">
+                <pre class="brush: css">
+                    .css{
+                        width: 16px;
+                        height: 16px;
+                        display: inline-block;
+                        margin-right: 5px;
+                        background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_kakao.png') 0 0 no-repeat;
+                        -webkit-background-size: 16px auto;
+                        background-size: 16px auto;
+                        vertical-align: middle;
+                    }
+                </pre>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="prefix">네이버 아이콘 심플형(갈색)</div>
+            <div class="used" style="margin:5px 0 15px 0; font-size:11px;">사용된 페이지 : 주문결제(현재운영) </div>
+            <div class="resultbox">
+                <span class="ico_naver_green"></span>
+            </div>
+            <div class="code-box">
+                <pre class="brush: css">
+                    .css{
+                        width: 16px;
+                        height: 16px;
+                        display: inline-block;
+                        margin-right: 5px;
+                        background: url('http://imagem.adidas.co.kr/images/adidas/ico_btn_naver.gif') 0 0 no-repeat;
+                        -webkit-background-size: 16px auto;
+                        background-size: 16px auto;
+                        vertical-align: middle;
                     }
                 </pre>
             </div>
